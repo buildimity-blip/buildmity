2ae2f3c82e4b97dc3f31243b64f4bca320a213d6
buildimity-blip <buildimity@gmail.com>
2026-04-04 13:37:55 +0300

    fix domain ALLOWED_HOSTS for production

--- service_app/settings.py
+++ service_app/settings.py
@@ -78,13 +78,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'service_app.wsgi.application'
 
+import os
+from pathlib import Path
+import dj_database_url
 
-# 🗄️ DATABASE (SQLite for local)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 
