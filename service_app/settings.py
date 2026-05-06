7826510e92ea40e9e6c9d5ed4d2ac632ef8187c8
buildimity-blip <buildimity@gmail.com>
2026-05-06 18:04:21 +0300

    Update settings.py

--- service_app/settings.py
+++ service_app/settings.py
@@ -1,5 +1,11 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
